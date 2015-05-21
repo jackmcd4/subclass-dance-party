c19990a440d98164f4c3dfdd4b24aab990b279c8
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-20 20:14:33 -0700

    Added other subclasses

--- src/MutantFish.js
+++ src/MutantFish.js
@@ -1,14 +1,44 @@
 var MutantFish = function(top, left, timeBetweenSteps){
   makeDancer.apply(this, arguments);
-  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-  //return blinkyDancer;
   this.$node.addClass('mutantFish');
   this.$node.append('<img src="images/3EyeFish.gif">');
 };
 
 MutantFish.prototype = Object.create(makeDancer.prototype);
-MutantFish.prototype.constructor = makeOtherDancer;
+MutantFish.prototype.constructor = MutantFish;
 
+var HomerTwirl = function(top, left, timeBetweenSteps){
+  makeDancer.apply(this, arguments);
+  this.$node.addClass('homerTwirl');
+  this.$node.append('<img src="images/homertwirl.gif">');
+};
+
+HomerTwirl.prototype = Object.create(makeDancer.prototype);
+HomerTwirl.prototype.constructor = HomerTwirl;
+
+var LisaDance = function(top, left, timeBetweenSteps){
+  makeDancer.apply(this, arguments);
+  this.$node.addClass('lisaDance');
+  this.$node.append('<img src="images/lisadance.gif">');
+};
+
+LisaDance.prototype = Object.create(makeDancer.prototype);
+LisaDance.prototype.constructor = LisaDance;
+
+var HomerHedge = function(top, left, timeBetweenSteps){
+  makeDancer.apply(this, arguments);
+  this.$node.addClass('homerHedge');
+  this.$node.append('<img src="images/homerhedge.gif">');
+};
+
+HomerHedge.prototype = Object.create(makeDancer.prototype);
+HomerHedge.prototype.constructor = HomerHedge;
+
+var Scream = function(top, left, timeBetweenSteps){
+  makeDancer.apply(this, arguments);
+  this.$node.addClass('scream');
+  this.$node.append('<img src="images/scream.gif">');
+};
+
+Scream.prototype = Object.create(makeDancer.prototype);
+Scream.prototype.constructor = Scream;
--- src/init.js
+++ src/init.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   window.dancers = [];
-
+  window.backgrounds = ['<img src="images/alcoholicCowboysBG.jpg">'];
   $(".addDancerButton").on("click", function(event){
     /* This function sets up the click handlers for the create-dancer
      * buttons on index.html. You should only need to make one small change to it.
